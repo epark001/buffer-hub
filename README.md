96316c3f97c54ccb0b315a552faa582e4c9a33a4
epark001 <sseiei001@gmail.com>
2020-07-25 18:15:02 -0500

    Restructuring files

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # buffer-hub
 
-conda env create --file=myfile.yaml 
+conda env create --file=environment.yml 
 cond activate buffer_hub 
 conda deactivate 
 pip install -r requirements.txt
+conda install -c conda-forge python
--- buffer_hub/urls.py
+++ buffer_hub/urls.py
@@ -1,7 +1,7 @@
 """buffer_hub URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from . import settings
 from temp.views import myView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', include('admin.urls')),
+    path('api/', include('api.urls')),
+    path('', include('frontend.urls')),
     path('temp/', myView)
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
