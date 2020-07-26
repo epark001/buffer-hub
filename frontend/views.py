fb43f557e30f743145d6546315866dc813fd0e5b
epark001 <sseiei001@gmail.com>
2020-07-26 03:16:05 -0500

    adding post request for signup

--- frontend/views.py
+++ frontend/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth import get_user_model, logout
 from django.contrib.auth import login as auth_login
 from admin.homepage.models import Homepage
+from django.db import connection
 import re
 
 # Create your views here.
@@ -24,36 +25,59 @@ def signup(request):
 
 def signup_post(request):
 	if request.method == 'POST':
+		new_user = {}
+		new_user['fname'] = request.POST['fname']
+		new_user['lname'] = request.POST['lname']
+		new_user['email_Id'] = request.POST['email_id']
+		new_user['password'] = request.POST['password']
 
-		fname = request.POST['fname']
-		lname = request.POST['lname']
-		email_id = request.POST['email_id']
-		password = request.POST['password']
-
-		if not re.match('^[(a-z)?(A-Z)?(0-9)?_?-?\.?\,?\s]+$',fname):
-
-			messages.error(request, 'Enter a valid Name')
-			return redirect('home-signup')
-		if not re.match('^[(a-z)?(A-Z)?(0-9)?_?-?\.?\,?\s]+$',lname):
+		if not re.match('^[(a-z)?(A-Z)?(0-9)?_?-?\.?\,?\s]+$',new_user['fname']):
 
 			messages.error(request, 'Enter a valid Name')
 			return redirect('home-signup')
+		if not re.match('^[(a-z)?(A-Z)?(0-9)?_?-?\.?\,?\s]+$',new_user['lname']):
 
-		if not re.match('^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$',email_id):
+			messages.error(request, 'Enter a valid Name')
+			return redirect('home-signup')
+
+		if not re.match('^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$',new_user['email_Id']):
 
 			messages.error(request, 'Enter a valid Email')
 			return redirect('home-signup')
 
 
-		print (fname,lname,email_id,password)
-		usr = CustomUser(name=name, email=email, password=make_password(password), usr_phone=number, usr_gender=gender)
+		print (new_user)
+		# usr = CustomUser(name=name, email=email, password=make_password(password), usr_phone=number, usr_gender=gender)
 
-		usr.save()
+		# usr.save()
+		with connection.cursor() as cursor:
+			cursor.execute("Insert Into User_Accounts(email_Id, password, fname, lname) Values (%s, %s, %s, %s)",
+            [new_user["email_Id"], new_user["password"], new_user["fname"], new_user["lname"]])
 
 		messages.success(request, 'Successfully Registered!')
 
 		return redirect('home-signup')
 
+'''
+def add_user_account(data):
+    with connection.cursor() as cursor:
+
+        if DEBUG:
+            print(data)
+
+        cursor.execute("Select * from User_Accounts where emailid = %s", [data["emailid"]])
+
+        row = cursor.fetchone()
+        # User Account already present
+        if row != None:
+            return False
+
+        cursor.execute("Insert Into User_Accounts Values (%s, %s, %s)",
+            [data["emailid"], data["passwd"], data["type_of_acc"]]
+        )
+
+        return True
+'''
 
 def login(request):
 	return render(request, 'frontendTemplates/login/index.html')
