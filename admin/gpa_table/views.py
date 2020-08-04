63d691dfdf9bb0549cb7f47891b08e0aa5a40e8b
epark001 <sseiei001@gmail.com>
2020-08-04 00:53:51 -0500

    Cleaning up backend and front end rest api connection

--- admin/gpa_table/views.py
+++ admin/gpa_table/views.py
@@ -16,29 +16,28 @@ def add(request):
 def save(request):
 
     if request.method == 'POST':
-        print ('here')
+
         field_id_val = str(uuid.uuid1())
-        course_comb_val = 'course_comb' in request.POST
-        subject_val = 'subject' in request.POST
-        number_val = 'number' in request.POST
-        course_title_val = 'course_title' in request.POST
+        course_comb_val = request.POST['course_comb']
+        subject_val = request.POST['subject']
+        number_val =  request.POST['number']
+        course_title_val =  request.POST['course_title']
 
-        a_plus_val = 'a_plus' in request.POST
-        a_standard_val = 'a_standard' in request.POST
-        a_minus_val = 'a_minus' in request.POST
-        b_plus_val = 'b_plus' in request.POST
-        b_standard_val = 'b_standard' in request.POST
-        b_minus_val = 'b_minus' in request.POST
-        c_plus_val = 'c_plus' in request.POST
-        c_standard_val = 'c_standard' in request.POST
-        c_minus_val = 'c_minus' in request.POST
-        d_plus_val = 'd_plus' in request.POST
-        d_standard_val = 'd_standard' in request.POST
-        d_minus_val = 'd_minus' in request.POST
-        f_val = 'f' in request.POST
-        average_grade_val = 'average_grade' in request.POST
-        primary_instructor_val = 'primary_instructor' in request.POST
-
+        a_plus_val = request.POST['a_plus']
+        a_standard_val =  request.POST['a_standard']
+        a_minus_val =  request.POST['a_minus']
+        b_plus_val =  request.POST['b_plus']
+        b_standard_val =  request.POST['b_standard']
+        b_minus_val =  request.POST['b_minus']
+        c_plus_val =  request.POST['c_plus']
+        c_standard_val =  request.POST['c_standard']
+        c_minus_val =  request.POST['c_minus']
+        d_plus_val =  request.POST['d_plus']
+        d_standard_val =  request.POST['d_standard']
+        d_minus_val =  request.POST['d_minus']
+        f_val =  request.POST['f']
+        average_grade_val =  request.POST['average_grade']
+        primary_instructor_val =  request.POST['primary_instructor']
 
         gpa_table_val = GpaTable(field_id=field_id_val, course_comb =course_comb_val,subject =subject_val, number = number_val, course_title =course_title_val, a_plus = a_plus_val, a_standard=a_standard_val,a_minus=a_minus_val,b_plus=b_plus_val, b_standard = b_standard_val, b_minus=b_minus_val, c_plus= c_plus_val,c_standard=c_standard_val,c_minus=c_minus_val,d_plus=d_plus_val,d_standard=d_standard_val, d_minus=d_minus_val, f=f_val, average_grade=average_grade_val, primary_instructor = primary_instructor_val)
 
