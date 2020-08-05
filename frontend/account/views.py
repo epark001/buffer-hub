99b76b467de07fdd2586bcca396389da0b171d53
edgarcuevas <edgarxcuevas@gmail.com>
2020-08-05 10:31:29 -0500

    Adding Ratchet Dropdowns

--- frontend/account/views.py
+++ frontend/account/views.py
@@ -57,6 +57,18 @@ def sqlsearch(request):
 	return render(request, 'frontendTemplates/account/sqlsearch.html', {'usr':usr})
 
 @login_required(login_url='home-login')
+def course_insert(request):
+	
+	usr = CustomUser.objects.filter(pk=request.user.id)
+
+	if not usr:
+		messages.error(request, 'Log In First!')
+		return redirect('home-login')
+	else:
+		usr = usr.get()
+
+	return render(request, 'frontendTemplates/account/course_insert.html', {'usr':usr})
+@login_required(login_url='home-login')
 def course_search(request):
 	
 	usr = CustomUser.objects.filter(pk=request.user.id)
