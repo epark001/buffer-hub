65f154bd8100aa15b13651149f1cece45748b803
epark001 <sseiei001@gmail.com>
2020-08-05 06:06:06 -0500

    connect autofill for user info on account overview

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('account/',        include('frontend.account.urls')),
     path('',                views.index,        name='home-index'),
+    # path('get-user')        views.get_user,     name='get-user'),
     path('signup/',         views.signup,       name='home-signup'),
     path('signup/post',     views.signup_post,  name='home-signup-post'),
     path('login/',          views.login,        name='home-login'),
@@ -14,6 +15,6 @@ urlpatterns = [
     path('demo-update/',    views.demo_update,  name='demo-update-post'),
     path('demo-delete/',    views.demo_delete,  name='demo-delete-post'),
     path('demo-site/',      views.demo_site,    name='demo-site-get'),
-    path('get-user/',       views.get_user,     name='get-user-post'),
+    path('get-user-info/',  views.get_user_info,name='get-user-info-post'),
     path('update-user/',    views.update_user,  name='update-user-post')
 ]
